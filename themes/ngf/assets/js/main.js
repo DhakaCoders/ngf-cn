660a30f207a44efb87e1766323d0af3950d35bef
niazmorshad <78914574+niazmorshad@users.noreply.github.com>
2021-06-25 23:37:30 +0600

    ..

--- themes/ngf/assets/js/main.js
+++ themes/ngf/assets/js/main.js
@@ -242,7 +242,26 @@ if( $('.plansGrdsSlider').length ){
       autoplaySpeed: 4000,
       speed: 700,
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
 }
 
@@ -255,7 +274,26 @@ if( $('.latestNewsGrdsSlider').length ){
       autoplaySpeed: 4000,
       speed: 700,
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint:600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
 }
 
