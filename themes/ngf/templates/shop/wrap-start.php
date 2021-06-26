17ce387f63021b2a8ff1a0333ec9fbe7d488a495
haque <azizul.softsw@gmail.com>
2021-06-26 11:47:11 +0600

    wc-shop wrap

---
+++ themes/ngf/templates/shop/wrap-start.php
@@ -0,0 +1,8 @@
+<section class="product-page-cntlr">
+	<div class="product-category">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<?php get_template_part('templates/shop', 'top');?>
+						<div class="fl-products-cntlr">
+							<div class="fl-product-top">';
