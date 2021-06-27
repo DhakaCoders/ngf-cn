615ea8ca1b2beaa28d424e449b08ff1b2cdecb4b
haque <azizul.softsw@gmail.com>
2021-06-27 13:01:10 +0600

    Product single

--- themes/ngf/templates/shop/loop.php
+++ themes/ngf/templates/shop/loop.php
@@ -15,34 +15,44 @@
     $terms = get_the_terms( $product->get_id(), 'product_cat' );
 ?>
 <div class="fl-product-grd mHc">
-    <?php if( !empty($seller_flash) ) printf('<div class="onsale-tag"><span>%s</span></div>', $seller_flash); ?>
-    <div class="fl-product-grd-inr">
-        <div class="fl-pro-grd-img-cntlr">
-            <a href="<?php echo get_permalink( $product->get_id() ); ?>" class="overlay-link"></a>
-            <div class="fl-pro-grid-img inline-bg" style="background-image: url('<?php echo $gridsrc; ?>');"></div>
-            <?php echo $gridtag; ?>
-        </div>
-        <div class="fl-pro-grd-des mHc1">
-            <h4 class="fl-h5 fl-pro-grd-title"><a href="<?php echo get_permalink( $product->get_id() ); ?>"><?php echo get_the_title(); ?></a></h4>
-            <div class="fl-pro-grd-price">
-            <span class="Vanaf">Vanaf</span><?php echo $product->get_price_html(); ?></div>
-            <?php if( !empty($short_desc) ) echo '<div class="fl-pro-grd-cont">'.wpautop($short_desc).'</div>'; ?>
-            <?php if( has_term( 41, 'product_cat', $product->get_id() ) ) { ?>
-                <div class="fl-pro-grd-btn koopnubtn">
-                <a href="<?php echo get_permalink( $product->get_id() ); ?>">
-                    <span>KOOP NU</span>
-                  </a>
-                </div>
-            <?php }else{ ?>
-                '<div class="fl-pro-grd-btn">
-                <a class="red-color-arrow-btn" href="<?php echo get_permalink( $product->get_id() ); ?>">
-                    <span><?php echo $label; ?></span>
-                    <i><svg class="red-right-arrow" width="7" height="11" viewBox="0 0 7 11">
-                    <use xlink:href="#red-right-arrow"></use> </svg>
-                    </i>
-                  </a>
-               </div>
-            <?php } ?>
-        </div>                    
+<div class="fl-pro-tag">
+  <span>NiEuW</span>  
+</div>
+<div class="fl-product-grd-inr">
+  <div class="fl-pro-grd-img-cntlr">
+    <a href="#" class="overlay-link"></a>
+    <div class="fl-pro-grid-img inline-bg" style="background-image: url('<?php echo THEME_URI; ?>/assets/images/pro-grid-img-01.jpg');"></div>
+  </div>
+  <div class="fl-pro-grd-des mHc1">
+    <h4 class="fl-h4 fl-pro-grd-title"><a href="#">Customized Training And Nutrition Plan</a></h4>
+    <div class="fl-pro-grd-price">
+      <span class="woocommerce-Price-amount amount">
+        <bdi><span class="woocommerce-Price-currencySymbol">€</span>90,00</bdi>
+      </span>
+      -
+      <span class="woocommerce-Price-amount amount">
+        <bdi><span class="woocommerce-Price-currencySymbol">€</span>90,00</bdi>
+      </span>                                                                   
     </div>
+    <div class="fl-pro-grd-details">
+      <p>Est in risus tempus egestas ut vitae lorem. Vel et elementum ultrices mauris dui auctor elit tellus vel. Nec donec a.</p>
+    </div>
+    <ul class="fl-pro-grd-cont">
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Suspendisse faucibus.</li>
+        <li>Tortor orci turpis nunc.</li>
+    </ul>
+    <div class="fl-pro-grd-btn">
+      <a class="fl-read-more-btn" href="#">
+        <span>READ MORE</span>
+        <i><svg class="dip-yellow-right-arrow" width="12" height="12" viewBox="0 0 12 12">
+        <use xlink:href="#dip-yellow-right-arrow"></use> </svg>
+        </i>
+      </a>
+    </div>
+  </div>
+  <div>
+    <a class="fl-black-btn fl-buy-now-btn" href="#">BUY NOW</a>
+  </div>                      
+</div>
 </div>
