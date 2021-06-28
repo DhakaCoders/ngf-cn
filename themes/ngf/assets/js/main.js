23267e27d590465788a1f1a4d742224c074bb205
niazmorshad <78914574+niazmorshad@users.noreply.github.com>
2021-06-28 19:09:23 +0600

    ..

--- themes/ngf/assets/js/main.js
+++ themes/ngf/assets/js/main.js
@@ -284,6 +284,12 @@ if( $('.wpforms-error').length ){
   });
 }
 
+$(".type-order-format .woocommerce-input-wrapper span").each(function(){
+      $(this).append('<div class="radio-custom"></div>')
+});
+$(".billing-address-wrap .same-as-shipping-address,.login-info p:first-child,.form-row .woocommerce-form__label-for-checkbox,#billing_personal_recom .woocommerce-input-wrapper,#billing_acttion_insp .woocommerce-input-wrapper").each(function(){
+      $(this).append('<div class="checkbox-custom"></div>')
+});
 
 /*start of Sabbir Hossain*/
 
