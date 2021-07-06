9cda675a38105bee928ef5cf1a8f32f6691777e7
Rannojit <cserannojitsarkar22@gmail.com>
2021-07-06 20:02:33 +0600

    .

--- themes/ngf/page-overons.php
+++ themes/ngf/page-overons.php
@@ -180,9 +180,9 @@ $thisID = get_the_ID();
                 <svg class="contact-from-info-rnd-bg" width="35" height="61" viewBox="0 0 35 61" fill="transparent">
                 <use xlink:href="#contact-from-info-rnd-bg"></use> </svg>
               </i>
-               <i class="contact-form-info-line-bg">
-                <svg class="contact-form-info-ln-bg" width="23" height="70" viewBox="0 0 23 70" fill="transparent">
-                <use xlink:href="#contact-from-info-line-bg"></use> </svg>
+               <i class="cta-line">
+                <svg class="cta-line-svg" width="32" height="89" viewBox="0 0 32 89" fill="transparent">
+                <use xlink:href="#cta-line-svg"></use> </svg>
               </i>
               <?php 
                   if( !empty($cta_sec['titel']) ) printf( '<h4 class="cta-title fl-h4">%s</h4>', $cta_sec['titel'] );
