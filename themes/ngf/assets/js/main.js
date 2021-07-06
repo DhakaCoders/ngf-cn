cd667bca42ce85174ec57534ad2da33e899613ed
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2021-07-06 15:27:36 +0600

    ,

--- themes/ngf/assets/js/main.js
+++ themes/ngf/assets/js/main.js
@@ -516,6 +516,42 @@ if( $('.blogDetailsSlider').length ){
 }
 
 
+if( $('.qty').length ){
+  $('.qty').each(function() {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.plus'),
+      btnDown = spinner.find('.minus'),
+      min = 1,
+      max = input.attr('max');
+
+    btnUp.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+  });
+
+}
+
+
 
 /*start of Jahir*/
 
