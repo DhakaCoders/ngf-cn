744d318ae76a22338df985e098015ad6b7126b1a
haque <azizul.softsw@gmail.com>
2021-07-06 22:59:15 +0600

    mini cart

--- themes/ngf/index.php
+++ themes/ngf/index.php
@@ -3,62 +3,10 @@ get_header();
 $thisID = get_option('page_for_posts');
 $intro = get_field('intro', $thisID);
 $page_title = !empty($intro['titel']) ? $intro['titel'] : get_the_title($thisID);
+$terms = get_terms( 'category', array(
+  'hide_empty' => false,
+) );
 ?>
-<section class="page-banner d-none">
-  <span class="page-bnr-skew"></span>
-  <span class="hdr-circle-icon"></span>
-  <div class="hdr-socials">
-    <span class="hdr-lft-line-icon">
-      <i><svg class="hdr-line-icon" width="58" height="95" viewBox="0 0 58 95" fill="#FFA800">
-      <use xlink:href="#hdr-line-icon"></use> </svg></i>
-    </span>
-    <ul class="reset-list">
-      <li>
-        <a target="_blank" href="#">
-          <i><svg class="facebook-icon" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
-            <use xlink:href="#facebook-icon"></use> </svg>
-          </i>
-        </a>
-      </li>
-      <li>
-        <a target="_blank" href="#">
-          <i><svg class="twiter-icon" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
-            <use xlink:href="#twiter-icon"></use> </svg>
-          </i>
-        </a>
-      </li>
-      <li>
-        <a target="_blank" href="#">
-          <i><svg class="linkden-icon" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
-            <use xlink:href="#linkden-icon"></use> </svg>
-          </i>
-        </a>
-      </li>
-      <li>
-        <a target="_blank" href="#">
-          <i><svg class="instagram-icon" width="24" height="24" viewBox="0 0 24 24" fill="#fff">
-            <use xlink:href="#instagram-icon"></use> </svg>
-          </i>
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div class="page-bnr-bg inline-bg" style="background: url('<?php echo THEME_URI; ?>/assets/images/has-transparent-bnr-img-002.jpg');"></div>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="page-bnr-desc-cntlr">
-          <div class="page-bnr-desc">
-            <span class="pg-bnr-desc-line"></span>
-            <span class="pg-bnr-desc-circle"></span>
-            <h1 class="fl-h1-80 pg-bnr-title">Eu ac urna<br> vel, sagittis<br> enim Quis.</h1>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
 <section class="sec-heading">
   <div class="container">
     <div class="row">
@@ -66,14 +14,19 @@ $page_title = !empty($intro['titel']) ? $intro['titel'] : get_the_title($thisID)
         <div class="sec-header">
           <h1 class="fl-h1 sec-title"><?php echo $page_title; ?></h1>
         </div>
+        <?php if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){ ?>
         <div class="fl-grid-category">
           <ul class="clearfix reset-list">
-            <li class="active"><a href="#">alle</a></li>
-            <li><a href="#">categorie</a></li>
-            <li><a href="#">categorie</a></li>
-            <li><a href="#">categorie</a></li>
+            <li class="active"><a href="<?php echo get_permalink($thisID); ?>"><?php _e( 'alle', 'ngf' ); ?></a></li>
+            <?php 
+              $i = 1; foreach ( $terms as $term ) { 
+              if($term->slug !='uncategorized'):
+            ?>
+            <li><a href="<?php echo esc_url( get_term_link( $term ) ); ?>"><?php echo $term->name; ?></a></li>
+            <?php endif; $i++; } ?>
           </ul>
         </div> 
+        <?php } ?>
       </div>
     </div>
   </div>
@@ -85,26 +38,40 @@ $page_title = !empty($intro['titel']) ? $intro['titel'] : get_the_title($thisID)
       <div class="col-md-12">         
         <div class="blog-grids-cntrl">
           <div class="blog-grid-items">
+            <?php if(have_posts()): ?>
             <ul class="clearfix reset-list">
+              <?php 
+                  while(have_posts()): the_post(); 
+                  global $post;
+                  $imgID = get_post_thumbnail_id(get_the_ID());
+                  $imgsrc = !empty($imgID)? cbv_get_image_src($imgID): '';
+                  $categories = get_the_terms( get_the_ID(), 'category' );
+              ?>              
               <li>                
                 <div class="blog-grid-item">                
                   <div class="blog-grid-img">                    
-                    <a href="#" class="overlay-link"></a>
-                    <div class="bgi-img inline-bg" style="background-image: url('<?php echo THEME_URI; ?>/assets/images/coach-fl-grid-img-01.jpg');">                  
+                    <a href="<?php the_permalink(); ?>" class="overlay-link"></a>
+                    <div class="bgi-img inline-bg" style="background-image: url('<?php echo $imgsrc; ?>');">                  
                     </div>
                   </div>  
+                  <?php if ( ! empty( $categories ) && ! is_wp_error( $categories ) ){ ?>
                   <div class="fl-grid-tag">
-                    <span>CATEGORIE</span>  
+                    <?php 
+                      foreach( $categories as $category ) {
+                        echo '<span>'.$category->name.'</span>';
+                      }
+                    ?>    
                   </div>
+                  <?php } ?>
                   <div class="blog-grid-des mHc">
                     <div class="blog-grid-des-inner">                                     
-                      <h4 class="fl-h4 bgi-title mHc1"><a href="#">Pellentesque tempus posuere urna tortor, sed. Lobortis.</a></h4>                      
+                      <h4 class="fl-h4 bgi-title mHc1"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>                      
                       <div class="bgi-des mHc2">
-                        <p>Est in risus tempus egestas ut vitae lorem. Vel et elementum ultrices mauris dui auctor elit tellus vel. Nec donec a.</p>
+                        <?php the_excerpt(); ?>
                       </div>    
                       <div class="fl-pro-grd-btn fl-btn-absolute">
-                        <a class="fl-read-more-btn" href="#">
-                          <span>READ MORE</span>
+                        <a class="fl-read-more-btn" href="<?php the_permalink(); ?>">
+                          <span><?php _e( 'READ MORE', 'ngf' ); ?></span>
                           <i><svg class="dip-yellow-right-arrow" width="12" height="12" viewBox="0 0 12 12">
                           <use xlink:href="#dip-yellow-right-arrow"></use> </svg>
                           </i>
@@ -114,160 +81,35 @@ $page_title = !empty($intro['titel']) ? $intro['titel'] : get_the_title($thisID)
                   </div>    
                 </div>  
               </li>
-              <li>                
-                <div class="blog-grid-item">                
-                  <div class="blog-grid-img">                    
-                    <a href="#" class="overlay-link"></a>
-                    <div class="bgi-img inline-bg" style="background-image: url('<?php echo THEME_URI; ?>/assets/images/coach-fl-grid-img-01.jpg');">                  
-                    </div>
-                  </div>  
-                  <div class="fl-grid-tag">
-                    <span>CATEGORIE</span>  
-                  </div>
-                  <div class="blog-grid-des mHc">
-                    <div class="blog-grid-des-inner">                                     
-                      <h4 class="fl-h4 bgi-title mHc1"><a href="#">Pellentesque tempus posuere urna tortor, sed. Lobortis.</a></h4>                      
-                      <div class="bgi-des mHc2">
-                        <p>Est in risus tempus egestas ut vitae lorem. Vel et elementum ultrices mauris dui auctor elit tellus vel. Nec donec a.</p>
-                      </div>    
-                      <div class="fl-pro-grd-btn fl-btn-absolute">
-                        <a class="fl-read-more-btn" href="#">
-                          <span>READ MORE</span>
-                          <i><svg class="dip-yellow-right-arrow" width="12" height="12" viewBox="0 0 12 12">
-                          <use xlink:href="#dip-yellow-right-arrow"></use> </svg>
-                          </i>
-                        </a>
-                      </div>
-                    </div>   
-                  </div>    
-                </div>  
-              </li>
-              <li>                
-                <div class="blog-grid-item">                
-                  <div class="blog-grid-img">                    
-                    <a href="#" class="overlay-link"></a>
-                    <div class="bgi-img inline-bg" style="background-image: url('<?php echo THEME_URI; ?>/assets/images/coach-fl-grid-img-01.jpg');">                  
-                    </div>
-                  </div>  
-                  <div class="fl-grid-tag">
-                    <span>CATEGORIE</span>  
-                  </div>
-                  <div class="blog-grid-des mHc">
-                    <div class="blog-grid-des-inner">                                     
-                      <h4 class="fl-h4 bgi-title mHc1"><a href="#">Pellentesque tempus posuere urna tortor, sed. Lobortis.</a></h4>                      
-                      <div class="bgi-des mHc2">
-                        <p>Est in risus tempus egestas ut vitae lorem. Vel et elementum ultrices mauris dui auctor elit tellus vel. Nec donec a.</p>
-                      </div>    
-                      <div class="fl-pro-grd-btn fl-btn-absolute">
-                        <a class="fl-read-more-btn" href="#">
-                          <span>READ MORE</span>
-                          <i><svg class="dip-yellow-right-arrow" width="12" height="12" viewBox="0 0 12 12">
-                          <use xlink:href="#dip-yellow-right-arrow"></use> </svg>
-                          </i>
-                        </a>
-                      </div>
-                    </div>   
-                  </div>    
-                </div>  
-              </li>
-              <li>                
-                <div class="blog-grid-item">                
-                  <div class="blog-grid-img">                    
-                    <a href="#" class="overlay-link"></a>
-                    <div class="bgi-img inline-bg" style="background-image: url('<?php echo THEME_URI; ?>/assets/images/coach-fl-grid-img-01.jpg');">                  
-                    </div>
-                  </div>  
-                  <div class="fl-grid-tag">
-                    <span>CATEGORIE</span>  
-                  </div>
-                  <div class="blog-grid-des mHc">
-                    <div class="blog-grid-des-inner">                                     
-                      <h4 class="fl-h4 bgi-title mHc1"><a href="#">Pellentesque tempus posuere urna tortor, sed. Lobortis.</a></h4>                      
-                      <div class="bgi-des mHc2">
-                        <p>Est in risus tempus egestas ut vitae lorem. Vel et elementum ultrices mauris dui auctor elit tellus vel. Nec donec a.</p>
-                      </div>    
-                      <div class="fl-pro-grd-btn fl-btn-absolute">
-                        <a class="fl-read-more-btn" href="#">
-                          <span>READ MORE</span>
-                          <i><svg class="dip-yellow-right-arrow" width="12" height="12" viewBox="0 0 12 12">
-                          <use xlink:href="#dip-yellow-right-arrow"></use> </svg>
-                          </i>
-                        </a>
-                      </div>
-                    </div>   
-                  </div>    
-                </div>  
-              </li>
-              <li>                
-                <div class="blog-grid-item">                
-                  <div class="blog-grid-img">                    
-                    <a href="#" class="overlay-link"></a>
-                    <div class="bgi-img inline-bg" style="background-image: url('<?php echo THEME_URI; ?>/assets/images/coach-fl-grid-img-01.jpg');">                  
-                    </div>
-                  </div>  
-                  <div class="fl-grid-tag">
-                    <span>CATEGORIE</span>  
-                  </div>
-                  <div class="blog-grid-des mHc">
-                    <div class="blog-grid-des-inner">                                     
-                      <h4 class="fl-h4 bgi-title mHc1"><a href="#">Pellentesque tempus posuere urna tortor, sed. Lobortis.</a></h4>                      
-                      <div class="bgi-des mHc2">
-                        <p>Est in risus tempus egestas ut vitae lorem. Vel et elementum ultrices mauris dui auctor elit tellus vel. Nec donec a.</p>
-                      </div>    
-                      <div class="fl-pro-grd-btn fl-btn-absolute">
-                        <a class="fl-read-more-btn" href="#">
-                          <span>READ MORE</span>
-                          <i><svg class="dip-yellow-right-arrow" width="12" height="12" viewBox="0 0 12 12">
-                          <use xlink:href="#dip-yellow-right-arrow"></use> </svg>
-                          </i>
-                        </a>
-                      </div>
-                    </div>   
-                  </div>    
-                </div>  
-              </li>
-              <li>                
-                <div class="blog-grid-item">                
-                  <div class="blog-grid-img">                    
-                    <a href="#" class="overlay-link"></a>
-                    <div class="bgi-img inline-bg" style="background-image: url('<?php echo THEME_URI; ?>/assets/images/coach-fl-grid-img-01.jpg');">                  
-                    </div>
-                  </div>  
-                  <div class="fl-grid-tag">
-                    <span>CATEGORIE</span>  
-                  </div>
-                  <div class="blog-grid-des mHc">
-                    <div class="blog-grid-des-inner">                                     
-                      <h4 class="fl-h4 bgi-title mHc1"><a href="#">Pellentesque tempus posuere urna tortor, sed. Lobortis.</a></h4>                      
-                      <div class="bgi-des mHc2">
-                        <p>Est in risus tempus egestas ut vitae lorem. Vel et elementum ultrices mauris dui auctor elit tellus vel. Nec donec a.</p>
-                      </div>    
-                      <div class="fl-pro-grd-btn fl-btn-absolute">
-                        <a class="fl-read-more-btn" href="#">
-                          <span>READ MORE</span>
-                          <i><svg class="dip-yellow-right-arrow" width="12" height="12" viewBox="0 0 12 12">
-                          <use xlink:href="#dip-yellow-right-arrow"></use> </svg>
-                          </i>
-                        </a>
-                      </div>
-                    </div>   
-                  </div>    
-                </div>  
-              </li>              
+              <?php endwhile; ?>             
             </ul>    
           </div>
+          <?php 
+          global $wp_query;
+          if( $wp_query->max_num_pages > 1 ): 
+          ?>
           <div class="fl-pagination-blog-cntrl">
             <div class="fl-pagination-ctlr">
-              <ul class="page-numbers">
-                <li><a class="prev page-numbers" href="#">←</a></li>
-                <li><span aria-current="page" class="page-numbers current">1</span></li>
-                <li><a class="page-numbers" href="#">2</a></li>
-                <li><a class="page-numbers" href="#">3</a></li>
-                <li><a class="page-numbers" href="#">4</a></li>
-                <li><a class="next page-numbers" href="#">→</a></li>
-              </ul>
+              <?php
+                $big = 999999999; // need an unlikely integer
+                $wp_query->query_vars['paged'] > 1 ? $current = $wp_query->query_vars['paged'] : $current = 1;
+
+                echo paginate_links( array(
+                  'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                  'type'      => 'list',
+                  'prev_text' => __('←'),
+                  'next_text' => __('→'),
+                  'format'    => '?paged=%#%',
+                  'current'   => $current,
+                  'total'     => $wp_query->max_num_pages
+                ) );
+              ?>
             </div>
           </div>  
+          <?php endif; ?>
+          <?php else: ?>
+            <div class="notfound"><?php _e( 'Geen resultaat', 'ngf' ); ?>.</div>
+          <?php endif; ?>
         </div>
       </div>  
     </div>
